504b3aa838f08885f488e063544074c286f400f6
Rick B <rick1stl@gmail.com>
2019-01-06 09:04:54 -0600

    RBB | ... | use momentJS

--- script.ts
+++ script.ts
@@ -1,42 +1,62 @@
 import * as fse from 'fs-extra';
+import moment from 'moment';
 
-const dirBase:string = 'C:/_MIS/github/home-automation-camera-ftp/';
+const timeNow = moment();
 
-let allJpgFiles:string[] = fse.readdirSync(dirBase)
-    .filter((file) => {
-        return file.split('.')[1] === 'jpg' ? true : false;
+const folders10daysFromNow: string[] = Array(10).fill('')
+    .map((item, index) => {
+        if (index === 0) {
+            item = timeNow.format().split('T')[0];
+        } else {
+            item = timeNow.add(1, 'day').format().split('T')[0];
+        }
+
+        return item;
+
     });
 
-console.log(allJpgFiles);
+console.log('folders10daysFromNow == ', folders10daysFromNow);
 
-// let allDaysFromFiles:string[] = allJpgFiles.
+const dirBase: string = 'C:/_MIS/github/home-automation-camera-ftp/test-image-dir/';
 
-// if(allJpgFiles.length > 0){
-    
-// }
+async function example(directory: string) {
+    try {
+        await fse.ensureDir(directory);
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+folders10daysFromNow.forEach((folderName, index) => {
+    example(`${dirBase}${folderName}`);
+});
 
 
 const file = fse.statSync(`${dirBase}rbb3_20190105_013719.jpg`);
 const fileModTimeMs = file.mtimeMs;
 const fileModTimeDateObj = new Date(fileModTimeMs);
 
-let YYYY = fileModTimeDateObj.getFullYear();
-let mmPrep = fileModTimeDateObj.getMonth() + 1;
-let MM = mmPrep < 10 ? `0${mmPrep}` : mmPrep;
-let ddPrep = fileModTimeDateObj.getDate();
-let DD = ddPrep < 10 ? `0${ddPrep}` : ddPrep;
+let allJpgFiles: string[] = fse.readdirSync(dirBase)
+    .filter((file) => {
+        // return true;
+        return file.split('.')[1] === 'jpg' ? true : false;
+    });
 
-let YYYYMMDD = `${YYYY}-${MM}-${DD}`;
+console.log(allJpgFiles);
 
-async function example(directory: string) {
-    try {
-        await fse.ensureDir(directory)
-        console.log('success!')
-    } catch (err) {
-        console.error(err)
-    }
-}
+allJpgFiles.forEach((fileName, index) => {
+    let date: string = fileName.split('_')[1];
+    let yyyy = date.substring(0, 4);
+    let mm = date.substring(4, 6);
+    let dd = date.substring(6, 8);
+    const dateFormatted:string = `${yyyy}-${mm}-${dd}`;
+    console.log('dateFormatted... ', dateFormatted);
+});
+
+// let allDaysFromFiles:string[] = allJpgFiles.
+
+// if(allJpgFiles.length > 0){
+
+// }
 
 
-
-example(`${dirBase}${YYYYMMDD}`);
